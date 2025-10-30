419f4e82d6fb03de25bc2985c79b3c9eef0565a1
eydbjorgham <eyha0001@stud.kea.dk>
2025-10-30 14:00:49 +0100

    init

--- src/app/components/LikeCounterExercise.jsx
+++ src/app/components/LikeCounterExercise.jsx
@@ -1,22 +1,31 @@
 "use client";
 // Importer CSS filen
 import "../component-style/like-counter-exercise.css";
-// TODO for studerende: Importer useState fra React
+import { useState } from 'react';
 
 export default function LikeCounterExercise() {
   // TODO for studerende: Opret state variabler
+  const [likes, setLikes] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
 
   // TODO for studerende: Implementer handleLike funktionen
-
+  const handleLike = () => {
+    if (!isLiked) {
+      setIsLiked(true);
+      setLikes(likes + 1);
+    } else {
+      setIsLiked(false);
+      // setLikes(likes - 1);
+    }
+  };
   return (
+
     <div className="exercise-container">
       <div className="post-card">
         {/* Header */}
         <div className="post-header">
           <h1 className="exercise-title">Like Counter Øvelse</h1>
-          <p className="exercise-description">
-            Få opslagets like-knap til at tælle likes
-          </p>
+          <p className="exercise-description">Få opslagets like-knap til at tælle likes</p>
         </div>
 
         {/* Post indhold */}
@@ -24,31 +33,30 @@ export default function LikeCounterExercise() {
           <div className="post-image">
             <span className="post-image-text">🎉</span>
           </div>
-          <p className="post-text">
-            Dette er et fantastisk opslag! Klik på like-knappen nedenfor for at
-            vise din støtte. Knappen skal skifte farve og tælleren skal
-            opdateres.
-          </p>
+          <p className="post-text">Dette er et fantastisk opslag! Klik på like-knappen nedenfor for at vise din støtte. Knappen skal skifte farve og tælleren skal opdateres.</p>
         </div>
 
         {/* Like sektion */}
         <div className="like-section">
           <button
             // TODO for studerende: Tilføj liked class hvis isLiked er true
-            className={`like-button`}
+            className={`like-button ${isLiked ? 'liked' : ''}`}
             // TODO for studerende: Tilføj onClick
+            // onClick={() => setIsLiked(!isLiked)}
+            onClick={handleLike}
           >
             {/* TODO for studerende: Vis det rigtige hjertet (❤️ / 🤍) i span elementet herunder  */}
-            <span className="heart-icon">🤍</span>
+            <span className="heart-icon">{isLiked ? '❤️' : '🤍'}</span>
             {/* TODO for studerende: Vis "Liked" eller "Like" */}
             {/* Hint: Brug en ternary operator */}
-            Like
+            {isLiked ? 'Liked' : 'Like'}
           </button>
 
           <div className="like-count">
             {/* TODO for studerende: Vis antallet af likes */}
             {/* Hint: Brug likes state variablen */}
-            likes 0
+            
+            likes {likes}
           </div>
         </div>
 
@@ -66,5 +74,6 @@ export default function LikeCounterExercise() {
         </div>
       </div>
     </div>
+
   );
 }
